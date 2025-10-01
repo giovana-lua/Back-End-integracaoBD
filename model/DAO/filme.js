d8ff81888018f5dd570a1230e5b7f0a897187170
GIOVANA BARBOSA SOUZA <25133664@cfp127aluno.local>
2025-10-01 17:01:35 -0300

    iniciando conteúdo novo

---
+++ model/DAO/filme.js
@@ -0,0 +1,93 @@
+/****************************************************************************************
+ * Objetivo: Arquivo responsável pela realização do CRUD no Banco de Dados MySQL
+ * Data: 01/10/2025
+ * Autor: Giovana
+ * Versão: 1.0
+ ****************************************************************************************/
+/**
+ * Depedencias do node para Banco de Dados Relacional
+ *      Sequelize -> foi uma biblioteca para acesso a banco de dados
+ *      Prisma    -> É uma biblioteca atual para acesso e manipulação de dados, utilizando SQL ou ORM (mySQL, PostegreSQL, SQLServer, Oracle)
+ *      Knex      -> É uma biblioteca atual para acesso e manipulação de dados, utilizando SQL
+ *      
+ * Dependencias do node para Bancos de dados NÃO Relacional
+ *      Mongoose  -> É uma biblioteca para acesso a banco de dados não relacional (MongoDB)
+ * 
+ * 
+ * Instalação do Prisma 
+ * npm install prisma --save                -> Permite a conexão com o BD
+ * npm install @prisma/client --save        -> Permite executar script SQL no BD
+ * 
+ ************************************************************************************************
+ *      $queryRawUnsafe()   -> Permite executar apenas scripts SQL que retornam 
+ *          dados do BD (SELECT), permite também executar um script SQL através de uma variável 
+ * 
+ *      $executeRawUnsafe() -> Permite executar scripts SQL que NÃO retornam dados 
+ *          do BD (INSERT, UPDATE, DELETE) 
+ * 
+ * ********************************************************************************************** 
+ *                       GARANTE A SEGURANÇA     (SQL INJECTION)
+ *      $queryRaw()   -> Permite executar apenas scripts SQL que retornam 
+ *          dados do BD (SELECT), permite APENAS executar um script SQL direto no método.
+ *          Permite também aplicar segurança contra SQL Injection
+ * 
+ *      $executeRaw() -> Permite executar scripts SQL que NÃO retornam dados 
+ *          do BD (INSERT, UPDATE, DELETE) permite APENAS executar um script SQL direto no método.
+ *          Permite também aplicar segurança contra SQL Injection
+ * 
+ *   
+ * 
+ */
+
+//Import da biblioteca do PrismaClient
+const { PrismaClient } = require('@prisma/client')
+
+//Cria um objeto do prisma client para manipular os scripts SQL
+const prisma = new PrismaClient()
+
+//Retorna todos os filmes do banco de dados 
+const getSelectAllFilms = async function(){
+    try {
+        //Script SQL 
+        let sql = `select * from tbl_filme order by id desc`
+
+        //Executa no BD o script SQL
+    let result = await prisma.$queryRawUnsafe(sql)
+    
+    if (result.lenght > 0 ) {
+        return result
+    }else{
+        return false
+    }
+        
+    } catch (error) {
+        //console.log(error) usar para achar o erro, se a API retornar erro 500, caso o erro for no banco de dados (se vira pra resolver kkkk)
+        return false
+    }
+
+    
+}
+
+//Retorna um filme filtrando pelo ID do banco de dados 
+const getSelectByIdFilms = async function(id){
+
+}
+
+//Insere um filme no banco de dados 
+const setInsertFilms = async function(filme){
+
+}
+
+//Atualiza um filme existente no bando de dados filrando pelo ID
+const setUpdateFilms = async function (filme) {
+    
+}
+
+//Apaga um filme existente no banco de dados filtrando pelo ID 
+const setDeleteFilms = async function(id){
+
+}
+
+module.exports = {
+    getSelectAllFilms
+}
